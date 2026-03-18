aa133b823637fdaf202f64326e64738734e9fb00
muslim1972 <muslimakkeel@gmail.com>
2026-03-18 07:22:18 +0300

    بالتوكل على الله نقطة / تضبيط منطق الاجازات واعادة الارصدة وخصمها (3) 18/3/2026 6:20 am

--- smart_leaves_logic.sql
+++ smart_leaves_logic.sql
@@ -121,7 +121,7 @@ BEGIN
     -- IF they canceled everything (actual is 0) we mark the leave as canceled entirely or keep cut
     IF p_actual_days = 0 THEN
         UPDATE public.leave_requests
-        SET hr_cut_status = 'approved', days_count = 0, status = 'canceled', leave_status = 'canceled'
+        SET hr_cut_status = 'approved', leave_status = 'cut' -- Keep it cut explicitly for reports (مقطوعة بالكامل)
         WHERE id = p_request_id;
         
         DELETE FROM public.leaves_details WHERE user_id = v_request.user_id AND start_date = v_request.start_date AND duration = v_request.days_count;
