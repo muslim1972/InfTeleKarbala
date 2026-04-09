3fe392505eb315d5f5127001ae62a26134754a7d
muslim1972 <muslimakkeel@gmail.com>
2026-04-09 23:30:56 +0300

    بعونه سبحانه و تعالى بداية وضع كل الاجراءات  لتشغيل اشعار الاتصال لايقاض الهاتف (2) 9/4/2026 11:30 م

--- android/app/src/main/java/com/inftelekarbala/app/MainActivity.java
+++ android/app/src/main/java/com/inftelekarbala/app/MainActivity.java
@@ -1,5 +1,25 @@
 package com.inftelekarbala.app;
 
+import android.os.Bundle;
+import android.view.WindowManager;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // Ensure the screen turns on and shows over the lock screen
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O_MR1) {
+            setShowWhenLocked(true);
+            setTurnScreenOn(true);
+        } else {
+            getWindow().addFlags(
+                WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
+                WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD |
+                WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON |
+                WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+            );
+        }
+    }
+}
